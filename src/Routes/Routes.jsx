1a655cc9f67cad4d8c4a8712f6c2b561e1380dd4
Rabbi Khan <Krabby2244@gmail.com>
2025-11-09 14:43:04 +0600

    Home file created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,25 +2,25 @@ import { createBrowserRouter } from "react-router";
 import MainLayout from "../Layout/MainLayout";
 import Register from "../Pages/Auth/Register";
 import Login from "../Pages/Auth/Login";
-
+import Home from "../Pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout/>,
-    // children: [
-    //     {
-    //         index:true,
-    //         element: 
-    //     }
-    // ]
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/auth/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/auth/register",
+        element: <Register></Register>,
+      },
+    ],
   },
-  {
-    path: '/auth/login',
-    element: <Login></Login>
-  },
-  {
-    path: '/auth/register',
-    element: <Register></Register>
-  }
 ]);
