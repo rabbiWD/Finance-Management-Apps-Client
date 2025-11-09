88ba2a4ed092c60f71dbd8bb0c68a9d5f8817bcb
Rabbi Khan <Krabby2244@gmail.com>
2025-11-09 14:05:10 +0600

    Mainlayout updated

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import NavBar from '../Components/NavBar/NavBar';
+import { Outlet } from 'react-router';
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className='max-w-7xl mx-auto'>
             <NavBar/>
+            <div className='mt-4'>
+                <Outlet></Outlet>
+            </div>
         </div>
     );
 };
