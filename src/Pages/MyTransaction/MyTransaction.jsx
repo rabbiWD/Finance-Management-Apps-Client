94abb731571cd6b6a822f8105236466186317383
Rabbi Khan <Krabby2244@gmail.com>
2025-11-11 01:09:35 +0600

    My transaction deleted part done

--- src/Pages/MyTransaction/MyTransaction.jsx
+++ src/Pages/MyTransaction/MyTransaction.jsx
@@ -1,54 +1,102 @@
-import React, { use, useEffect, useState } from 'react';
-import { AuthContext } from '../../Context/AuthContext';
-import LoadingSpinner from '../../Components/LoadingSpinner/LoadingSpinner';
+import React, { use, useEffect, useState } from "react";
+import { AuthContext } from "../../Context/AuthContext";
+import LoadingSpinner from "../../Components/LoadingSpinner/LoadingSpinner";
+import { Link } from "react-router";
+import Swal from "sweetalert2";
 
 const MyTransaction = () => {
-    const {user} = use(AuthContext)
-    const [transactions, setTransactions] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const { user } = use(AuthContext);
+  const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
-        if(user?.email){
-            fetch(`http://localhost:3000/my-transaction?email=${user.email}`)
-            .then(res => res.json())
-            .then((data)=>{
-                console.log(data);
-                setTransactions(data);
-                setLoading(false);
-            })
-            .catch((error)=>{
-                console.log(error);
-            })
-        }
-    }, [user]);
-
-    if(loading){
-        return <LoadingSpinner/>
+  useEffect(() => {
+    if (user?.email) {
+      fetch(`http://localhost:3000/my-transaction?email=${user.email}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setTransactions(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
+  }, [user]);
 
-    return (
-        <div>
-            <h2>My Transaction</h2>
+//   delete transaction
+   const handleDelete = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`https://3d-model-hub-server-three.vercel.app/models/${model._id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            // navigate("/all-models");
 
-            {
-                transactions.length === 0 ? (
-                    <p>Transaction Not Found</p>
-                ) : (
-                    <div>
-                        {
-                            transactions.map((transaction) => (
-                                <div key={transaction._id}>
-                                    <h2>{transaction.category}</h2>
-                                    <p>Type: {transaction.type}</p>
-                                    <p>Date: {new Date(transaction.date).toLocaleDateString()}</p>
-                                </div>
-                            ))
-                        }
-                    </div>
-                )
-            }
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    });
+  };
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <div className="max-w-7xl mx-auto px-4 py-10">
+      <h2 className="text-2xl font-bold text-center mb-5">My Transaction</h2>
+
+      {transactions.length === 0 ? (
+        <p>Transaction Not Found</p>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {transactions.map((transaction) => (
+            <div
+              key={transaction._id}
+              className='p-5 rounded-xl shadow-md border transition-transform hover:scale-[1.02]' 
+            //     ${
+            //     transaction.type === "Income"
+            //       ? "bg-green-50 border-green-300"
+            //       : "bg-red-50 border-red-300"
+            //   }`
+            >
+              <h2 className="text-xl font-bold mb-3 text-gray-700">Type: {transaction.type}</h2>
+              <h2 className="text-gray-700">Category: {transaction.category}</h2>
+              <p className="text-gray-700">Amount: {transaction.amount}</p>
+              <p className="text-gray-700 mb-5">Date: {new Date(transaction.date).toLocaleDateString()}</p>
+              <div className="flex justify-center gap-5 mt-4">
+                <Link to={`/update/${transaction._id}`} className="btn btn-secondary p-5 rounded-lg text-base">Update</Link>
+                <button onClick={handleDelete} className="btn btn-outline p-5 rounded-lg text-base">Delete</button>
+                <Link to={`/transaction-details`} className="btn btn-primary p-5 rounded-lg text-base">View Details</Link>
+              </div>
+            </div>
+          ))}
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default MyTransaction;
